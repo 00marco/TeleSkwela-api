1c1ae540725aec3bd6794e09ab2775bd39f8f7f6
Marco <marcopulido00@gmail.com>
2020-06-23 15:21:13 +0800

    added models to django admin, added model relationships

--- teleskwela_api/rest/admin.py
+++ teleskwela_api/rest/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User, Award, Lesson, Quiz, Subject, SubjectCategory
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Award)
+admin.site.register(Lesson)
+admin.site.register(Quiz)
+admin.site.register(Subject)
+admin.site.register(SubjectCategory)
--- teleskwela_api/rest/models.py
+++ teleskwela_api/rest/models.py
@@ -3,19 +3,48 @@ from django.db import models
 # Create your models here.
 class User(models.Model):
     first_name = models.CharField(max_length=100)
+    awards = models.ManyToManyField(Award)
+    subjects = models.ManyToManyField(Subject)
+
+    def __str__(self):
+        return first_name
+
 
 class Award(models.Model):
     name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return name
+
 
 class SubjectCategory(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return name
+
+
 class Subject(models.Model):
     name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    subjectCategory = models.ForeignKey(SubjectCategory, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return name
+
 
 class Lesson(models.Model):
     name = models.CharField(max_length=100)
+    subject = models.ForeignKey(Subject, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return name
+
 
 class Quiz(models.Model):
     name = models.CharField(max_length=100)
+    lesson = models.ForeignKey(Lesson, on_delete = models.CASCADE)
 
+    def __str__(self):
+        return name
