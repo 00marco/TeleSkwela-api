5ef3e2456df73b8e9217fc733cdc3b22daa3ee9c
Marco <marcopulido00@gmail.com>
2020-06-25 23:57:22 +0800

    added score and total number of items fields to Quiz model

--- teleskwela_api/rest/models/quiz.py
+++ teleskwela_api/rest/models/quiz.py
@@ -3,7 +3,11 @@ from rest.models.lesson import Lesson
 
 class Quiz(models.Model):
     name = models.CharField(max_length=100)
+    score = models.IntegerField()
+    total_number_of_items = models.IntegerField(required=True)
+
     lesson = models.ForeignKey(Lesson, on_delete = models.CASCADE)
+
 
     def __str__(self):
         return self.name
