e41303755a6d12348bd92eb8eafd805ccb32aa9a
Marco <marcopulido00@gmail.com>
2020-06-26 00:01:55 +0800

    added last name and year level field to student model and created migration files

--- teleskwela_api/rest/models/student.py
+++ teleskwela_api/rest/models/student.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
-
+    last_name = models.CharField(max_length=100)
+    year_level = models.CharField(max_length=100)
+    
     def __str__(self):
         return self.first_name
